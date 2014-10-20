376dd8167e4cb8ed414be20b279bdb5a057f93c9
AZ technology <aztec@localhost.domain>
2014-10-20 16:48:45 +0800

    Include emmcutils

--- Android.mk
+++ Android.mk
@@ -116,4 +116,5 @@ include $(LOCAL_PATH)/minui/Android.mk \
     $(LOCAL_PATH)/tools/Android.mk \
     $(LOCAL_PATH)/edify/Android.mk \
     $(LOCAL_PATH)/updater/Android.mk \
+    $(LOCAL_PATH)/emmcutils/Android.mk	\
     $(LOCAL_PATH)/applypatch/Android.mk
